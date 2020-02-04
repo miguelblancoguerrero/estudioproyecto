8841de3dddbf357cfa6f475e6836e70e77c50190
JuanRobles2164 <juanse2164@hotmail.com>
2020-02-03 20:31:55 -0500

    Update funciones.js

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -12,15 +12,29 @@ $('#nit_cliente').keyup(function(e){
         success:function(response){
             let data = $.parseJSON(response);
             if(response != 0){
+                //Asignar los valores
                 $('#nombreCliente').val(data.nombre);
                 $('#apellidoCliente').val(data.apellidos);
                 $('#direccionCliente').val(data.direccion);
                 $('#telefonosCliente').val(data.telefonos);
+
+                //Anular la edición de campos
+                $('#nombreCliente').attr('disabled','disabled');
+                $('#apellidoCliente').attr('disabled','disabled');
+                $('#direccionCliente').attr('disabled','disabled');
+                $('#telefonosCliente').attr('disabled','disabled');
+
+
             }else{
                 $('#nombreCliente').val('');
                 $('#apellidoCliente').val('');
                 $('#direccionCliente').val('');
                 $('#telefonosCliente').val('');
+
+                $('#nombreCliente').removeAttr('disabled');
+                $('#apellidoCliente').removeAttr('disabled');
+                $('#direccionCliente').removeAttr('disabled');
+                $('#telefonosCliente').removeAttr('disabled');
             } 
         },
         error:function(error){
